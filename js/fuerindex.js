edc3d32af101e27cc099a43ef4e49278976d1d7e
GreekFellows <unknownuser826@gmail.com>
2015-03-14 18:13:06 +0800

    new frontpage
    cxefpagxo!

--- js/fuerindex.js
+++ js/fuerindex.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-	$("p").click(function(event) {
-		alert("yeah you clicked on some paragraph");
-	})
+	$("#to00").click(function(event) {
+		$("#div00").fadeIn("fast");
+	});
+
+	$("#to01").click(function(event) {
+		$("#div00").fadeOut("fast");
+	});
+
+	$("#to02").click(function(event) {
+		$("#div00").fadeOut("fast");
+	});
+
+	$("#to03").click(function(event) {
+		$("#div00").fadeOut("fast");
+	});
+
+	$("#to04").click(function(event) {
+		$("#div00").fadeOut("fast");
+	});
 });
